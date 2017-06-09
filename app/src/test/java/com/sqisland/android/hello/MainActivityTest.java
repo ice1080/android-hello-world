c7e8ef3705c8f8c3ed921910b73bc032d70cdc43
ihudson <ice1080@gmail.com>
2017-06-08 23:29:38 -0600

    Attempting to do a test but it didn't work cuz espresso isn't in 26

---
+++ app/src/test/java/com/sqisland/android/hello/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.sqisland.android.hello;
+
+import junit.framework.TestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ihudson on 6/8/17.
+ */
+@RunWith(JUnit4.class)
+public class MainActivityTest extends TestCase {
+    
+}
