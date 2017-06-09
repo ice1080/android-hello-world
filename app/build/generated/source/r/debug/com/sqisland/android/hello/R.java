bd474001c89ab6c4d6f658047176ab185b92bade
ihudson <ice1080@gmail.com>
2017-06-08 22:46:49 -0600

    Prepping style files

--- app/build/generated/source/r/debug/com/sqisland/android/hello/R.java
+++ app/build/generated/source/r/debug/com/sqisland/android/hello/R.java
@@ -10,6 +10,10 @@ package com.sqisland.android.hello;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
@@ -17,7 +21,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+    }
+    public static final class style {
+        public static final int AppTheme=0x7f040000;
     }
 }
